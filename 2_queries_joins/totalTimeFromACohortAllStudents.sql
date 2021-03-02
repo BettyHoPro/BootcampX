8919698ad7b84ccbcd76fba99c3606ea4be7966a
BettyHoPro <bettyvisualtech@gmail.com>
2021-03-02 05:59:42 +0000

    update totalTimeFromACohortAllStudents.sql

--- 2_queries_joins/totalTimeFromACohortAllStudents.sql
+++ 2_queries_joins/totalTimeFromACohortAllStudents.sql
@@ -1,4 +1,4 @@
-SELECT sum(assignment_submissions.duration) 
+SELECT sum(assignment_submissions.duration) as  total_duration
 FROM  students 
 JOIN assignment_submissions on students.id = assignment_submissions.student_id 
 JOIN cohorts on cohorts.id = students.cohort_id
